2ed96e29ae381c8b0d0753c6e3bf4c0a4cb676f6
KokoYuardiA <11201048@student.itk.ac.id>
2022-12-15 01:07:27 +0800

    update search (lagi)

--- src/components/Search.jsx
+++ src/components/Search.jsx
@@ -13,16 +13,30 @@ const Search = ({ state, setSearchResults }) => {
     }
     const debounceOnchange = debounce(handleSearchChange, 500)
     return (
-        <header>
-            <form className='search' onSubmit={handleSubmit}>
-                <input 
-                    className='search_input'
+        <form className="max-w-sm px-4" onSubmit={handleSubmit}>
+            <div className="relative">
+                <svg
+                    xmlns="http://www.w3.org/2000/svg"
+                    className="absolute top-0 bottom-0 w-6 h-6 my-auto text-gray-400 left-3"
+                    fill="none"
+                    viewBox="0 0 24 24"
+                    stroke="currentColor"
+                >
+                <path
+                    strokeLinecap="round"
+                    strokeLinejoin="round"
+                    strokeWidth={2}
+                    d="M21 21l-6-6m2-5a7 7 0 11-14 0 7 7 0 0114 0z"
+                />
+                </svg>
+                <input
                     type="text"
-                    id="search"
+                    placeholder="Search by Name"
+                    className="w-full py-3 pl-12 pr-4 text-gray-500 border rounded-md outline-none bg-gray-50 focus:bg-white focus:border-indigo-600"
                     onChange={debounceOnchange}
                 />
-            </form>
-        </header>
+            </div>
+        </form>
     )
 }
 
