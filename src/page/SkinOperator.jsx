1cc86cf2d322662a70c1ae57e760cfb28cf7e313
Dasewasia <77565642+Dasewasia10@users.noreply.github.com>
2022-12-15 21:05:11 +0800

    Minor Update

--- src/page/SkinOperator.jsx
+++ src/page/SkinOperator.jsx
@@ -21,52 +21,54 @@ const SkinOperator = () => {
 
   return (
     <div className="mx-20 my-10">
-      <h1 className="mb-10">Link to this page : {window.location.pathname}</h1>
       <div>
-        <div>
+        <div
+          id="arknightslogo"
+          className="flex relative lg:fixed mt-5 mb-10 top-2 items-center justify-center lg:justify-start"
+        >
           <img
-            className="absolute mx-auto left-20 w-auto h-24 top-10"
+            className="w-auto h-24"
             src="https://webusstatic.yo-star.com/ark_us_web/pc/img/logo02.924e2f2a.png"
             alt="Arknights_White_Logo"
           />
         </div>
-        <Link to="/gallery">
-          <p className="absolute bottom-5 mt-5 mb-10 text-center text-2xl bg-[#010440] p-2 rounded-xl border-2 border-[#D9D9D9] hover:bg-[#D9D9D9] hover:text-[#010440] hover:border-[#010440]">
-            {"< "}Kembali
-          </p>
+        <Link to="/detail">
+          <div className="flex fixed mb-10 bottom-2">
+            <p className="text-center text-lg lg:text-2xl bg-[#010440] p-2 rounded-xl border-2 border-[#D9D9D9] hover:bg-[#D9D9D9] hover:text-[#010440] hover:border-[#010440]">
+              {"< "}Kembali
+            </p>
+          </div>
         </Link>
         {operator.length &&
           operator.map((op) => {
             if (op._id === id) {
               return (
-                <div className="flow-root">
+                <div className="relative grid overflow-hidden grid-cols-2 auto-rows-auto gap-5 top-1/2 left-0 right-0">
                   <div
-                    className="float-left grid grid-rows-3 grid-cols-2"
-                    id="list-of-skin"
+                    id="opskin"
+                    className="flex flex-col items-center justify-center"
                   >
                     {Object.entries(op.art).map(([key, value]) => {
                       return (
-                        <div id="opskin">
-                          <a href={value}>
-                            <p>{key}</p>
-                          </a>
-                          <br />
+                        <div className="flex flex-row">
+                          <div className="flex absolute text-center text-lg lg:text-2xl bg-[#010440] p-2 rounded-xl border-2 border-[#D9D9D9] hover:bg-[#D9D9D9] hover:text-[#010440] hover:border-[#010440]">
+                            <a href={value}>
+                              <p>{key}</p>
+                            </a>
+                            <br />
+                          </div>
+                          <div className="">
+                            <img
+                              className="w-96 h-auto"
+                              key={key}
+                              src={value}
+                              alt="artBase"
+                            />
+                          </div>
                         </div>
                       );
                     })}
                   </div>
-                  {Object.entries(op.art).map(([key, value]) => {
-                    return (
-                      <div className="relative flex flex-row-reverse float-right" id="gambar">
-                        <img
-                          className="w-96 h-auto"
-                          key={key}
-                          src={value}
-                          alt="artBase"
-                        />
-                      </div>
-                    );
-                  })}
                 </div>
               );
             }
