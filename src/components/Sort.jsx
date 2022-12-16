361e42555ef419aaf66001f4c262019cd528560a
Dasewasia <77565642+Dasewasia10@users.noreply.github.com>
2022-12-17 02:03:53 +0800

    minor changes

--- src/components/Sort.jsx
+++ src/components/Sort.jsx
@@ -1,29 +1,59 @@
 import React from "react";
-import debounce from "lodash.debounce";
 
-const Sort = ({ state, setSearchResults }) => {
-  const handleSubmit = (e) => e.preventDefault();
-
-  const handleSearchChange = (e) => {
-    if (!e.target.value) return setSearchResults(state);
-
-    const resultArray = state.filter((state) =>
-      state.name.toLowerCase().includes(e.target.value.toLowerCase())
-    );
-
-    setSearchResults(resultArray);
-  };
-  const debounceOnchange = debounce(handleSearchChange, 500);
+const Sort = () => {
   return (
     <header>
-      <form className="search" onSubmit={handleSubmit}>
-        <input
-          className="search_input"
-          type="text"
-          id="search"
-          onChange={debounceOnchange}
-        />
-      </form>
+      <div class="relative">
+        <input type="checkbox" id="sortbox" class="hidden absolute" />
+        <label
+          for="sortbox"
+          class="flex items-center space-x-1 cursor-pointer rounded-xl border-2 p-2 border-[#D9D9D9] hover:bg-[#D9D9D9] hover:text-[#010440]"
+        >
+          <span class="text-lg">Sort By</span>
+          <svg
+            class="h-4 w-4"
+            xmlns="http://www.w3.org/2000/svg"
+            fill="none"
+            viewBox="0 0 24 24"
+            stroke="currentColor"
+          >
+            <path
+              stroke-linecap="round"
+              stroke-linejoin="round"
+              stroke-width="2"
+              d="M19 9l-7 7-7-7"
+            />
+          </svg>
+        </label>
+
+        <div
+          id="sortboxmenu"
+          class="absolute mt-1 right-1 top-full min-w-max shadow rounded opacity-0 bg-gray-300 border border-gray-400 transition delay-75 ease-in-out z-10"
+        >
+          <ul class="block text-right text-gray-900">
+            <li>
+              <a href="#" class="block px-3 py-2 hover:bg-gray-200">
+                Featured
+              </a>
+            </li>
+            <li>
+              <a href="#" class="block px-3 py-2 hover:bg-gray-200">
+                Newest
+              </a>
+            </li>
+            <li>
+              <a href="#" class="block px-3 py-2 hover:bg-gray-200">
+                Price: Low to High
+              </a>
+            </li>
+            <li>
+              <a href="#" class="block px-3 py-2 hover:bg-gray-200">
+                Price: High to Low
+              </a>
+            </li>
+          </ul>
+        </div>
+      </div>
     </header>
   );
 };
--- src/page/Card.jsx
+++ src/page/Card.jsx
@@ -3,6 +3,8 @@ import { Link } from "react-router-dom";
 import { getData } from "../api/axios";
 import Search from "../components/Search";
 import ListOperator from "../components/ListOperator";
+import Sort from "../components/Sort";
+import Filter from "../components/Filter";
 
 const Card = () => {
   const [state, setState] = useState([]);
@@ -34,33 +36,35 @@ const Card = () => {
         />
       </div>
       <div className="flex flex-col lg:flex-row my-10 justify-center">
-        <Link to="/choosemenu">
-          <p
-            className="text-center text-2xl bg-[#010440] p-2 rounded-xl border-2 border-[#D9D9D9] hover:bg-[#D9D9D9] hover:text-[#010440] hover:border-[#010440]"
-            title="Kembali"
-            id="kembali"
-          >
-            {"< "}
-          </p>
-        </Link>
-        <div className="flex ml-0 mt-3 lg:mt-0 lg:ml-5 items-center justify-center">
-          <Search state={state} setSearchResults={setSearchResults} />
+        <div className="flex flex-row items-center justify-center">
+          <Link to="/choosemenu">
+            <p
+              className="text-center text-2xl bg-[#010440] p-2 rounded-xl border-2 border-[#D9D9D9] hover:bg-[#D9D9D9] hover:text-[#010440] hover:border-[#010440]"
+              title="Kembali"
+              id="kembali"
+            >
+              {"< "}
+            </p>
+          </Link>
+          <div className="flex ml-0 mt-3 lg:mt-0 lg:ml-5 items-center justify-center">
+            <Search state={state} setSearchResults={setSearchResults} />
+          </div>
         </div>
         <div className="flex ml-0 mt-3 lg:mt-0 lg:ml-5 items-center justify-center">
           <p>atau</p>
         </div>
         <div className="flex ml-0 mt-3 lg:mt-0 lg:ml-5 items-center justify-center">
-          <Search state={state} setSearchResults={setSearchResults} />
+          <Filter />
         </div>
         <div className="flex ml-0 mt-3 lg:mt-0 lg:ml-5 items-center justify-center">
           <p>atau</p>
         </div>
         <div className="flex ml-0 mt-3 lg:mt-0 lg:ml-5 items-center justify-center">
-          <Search state={state} setSearchResults={setSearchResults} />
+          <Sort />
         </div>
       </div>
 
-      <div className="flex mt-10 mb-5 justify-center lg:justify-start text-xl">
+      <div className="flex mt-10 mb-5 justify-center text-xl">
         <p>Newest Operator</p>
       </div>
       <ListOperator
